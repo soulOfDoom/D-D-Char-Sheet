2af4458b5f9246e0c6736f0d77697a40b195b918
Max Huber <soulnom@gmail.com>
2013-02-28 14:49:26 -0600

    Initial Commit

---
+++ D&D Char Sheet/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  D&D Char Sheet
+//
+//  Created by Max Huber on 2/28/13.
+//  Copyright (c) 2013 IDEAS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
